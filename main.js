58f80df32e1dadfc3719afe56f29f223113ca278
Rakhat <rakhat.zhussupkhanov@nu.edu.kz>
2022-10-25 13:19:49 +0600

    Lab9 Problems 1, 3, 11

---
+++ main.js
@@ -0,0 +1,21 @@
+//problem 1
+function hw () {
+  return 'Hello, World!\nHello, World!\nHello, World!'
+}
+
+//problem 11
+function isEven(num1) {
+  if (num1 % 2 == 0) {
+    return 'Even'
+  }
+  else{return 'Odd'}
+}
+
+//problem 3
+function circle_area(r) {
+  return 3.14 * r**2
+}
+
+console.log(hw());
+console.log(isEven(7));
+console.log(circle_area(5))
